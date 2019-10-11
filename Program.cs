fd73e5de2ffafad10ab169113c70f0caaf9aee18
Le Shred <fred777@free.fr>
2019-10-12 01:03:57 +0200

    Save logs in logs folder

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace IniFileParser.Example
     public class MainProgram
     {
         private static Process Process;
-        private static DateTime _nextUpdate;
+        private static string _path;
 
         public static void Main()
         {
@@ -27,13 +27,12 @@ namespace IniFileParser.Example
 
             IniData parsedData = fileIniData.ReadFile("Configuration.ini");
 
-
             string[] reboot = parsedData["GeneralConfiguration"]["reboot"].Split(',');
-            string path = parsedData["GeneralConfiguration"]["path"].ToString();
+            _path = parsedData["GeneralConfiguration"]["path"].ToString();
             
             ProcessStartInfo startInfo = new ProcessStartInfo("server.exe");
             startInfo.FileName = "server.exe";
-            startInfo.WorkingDirectory = path;
+            startInfo.WorkingDirectory = _path;
             startInfo.WindowStyle = ProcessWindowStyle.Normal;
 
             try
@@ -47,7 +46,7 @@ namespace IniFileParser.Example
 
             while (true)
             {
-                if (Process != null && DateTime.Now > _nextUpdate)
+                if (Process != null)
                 {
                     foreach (var rebootHours in reboot)
                     {
@@ -55,22 +54,56 @@ namespace IniFileParser.Example
                         {
                             Console.WriteLine($"{DateTime.Now} | Reboot de {rebootHours} en cours");
                             Process.Kill();
+                            SaveLog();
                             Process.Start();
-                            _nextUpdate = DateTime.Now.AddMinutes(1);
+                            Thread.Sleep(1000);
                             return;
                         }
                     }
                     
                     if (Process.HasExited)
                     {
-                        Console.WriteLine($"{DateTime.Now} | Crash détecter!");
+                        Console.WriteLine($"{DateTime.Now} | Crash détecté!");
+                        SaveLog();
                         Process.Start();
                     }
 
-                    _nextUpdate = DateTime.Now.AddMinutes(1);
+                    Thread.Sleep(1000);
                 }
             }
         }
 
+        private static void SaveLog()
+        {
+            string logFile = $@"{_path}\server.log";
+
+            while (IsFileLocked(logFile))
+                Thread.Sleep(100);
+
+            Directory.CreateDirectory($@"{_path}\logs");
+            File.Move(logFile, $@"{_path}\logs\{DateTime.Now.ToString("yyyyMMdd_HHmmss")}_server.log");
+        }
+
+        private static bool IsFileLocked(string filePath)
+        {
+            FileInfo file = new FileInfo(filePath);
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            return false;
+        }
     }
 }
